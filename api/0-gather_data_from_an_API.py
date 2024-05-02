0fcfacb8615dc6636989cf871c0eb1789f39327f
nataly <ncristina.rodriguezr@gmail.com>
2024-05-02 10:09:33 -0500

    point 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
+Script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
 import requests
 from sys import argv
